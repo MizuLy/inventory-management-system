e9dd6f89b218004d63886cb63bab60d5b9f48f4c
MizuLy <optionalsa@gmail.com>
2026-04-18 11:36:21 +0700

    Add: daisyui theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function App() {
-  return <div>Hello mang</div>;
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <button
+        onClick={() => console.log("Hehe")}
+        className="bg-white/5 border border-white/10 backdrop-blur-md h-[64px] rounded-full px-6 md:px-8 flex justify-between items-center shadow-2xl outline-none"
+      >
+        Hehe
+      </button>
+    </div>
+  );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -29,4 +29,7 @@ export default {
     },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: true,
+  },
 };
